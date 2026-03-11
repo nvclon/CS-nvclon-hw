312f9af681773b622ae766241d49ec3d2dcf220e
vetoshkin.is <114411040+nvclon@users.noreply.github.com>
2026-03-11 20:37:57 +0300

    completed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CS-nvclon-hw
 Homeworks C# Ivan Vetoshkin
+
+# How to run
+
+```bash
+cd homework_1/calc
+dotnet run
+```
--- homework_1/calc/Program.cs
+++ homework_1/calc/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Calculator {
+    static void Main() {
+        Console.Write("Первое число: ");
+        double num1;
+        while (!double.TryParse(Console.ReadLine(), out num1)) {
+            Console.Write("Ошибка. Введите число: ");
+        }
+
+        Console.Write("Второе число: ");
+        double num2;
+        while (!double.TryParse(Console.ReadLine(), out num2)) {
+            Console.Write("Ошибка. Введите число: ");
+        }
+
+        Console.WriteLine("Теперь операция: +, -, *, /");
+        string? operation = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(operation)) {
+            Console.WriteLine("Неверная операция!");
+            return;
+        }
+
+        double result = 0;
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    Console.WriteLine("Undefined!");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Неверная операция!");
+                return;
+        }
+        Console.WriteLine("Результат: " + result);
+        Console.WriteLine("Нажмите Enter, чтобы выйти...");
+        Console.ReadLine();
+    }
+}
